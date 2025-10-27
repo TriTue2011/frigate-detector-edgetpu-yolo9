fdbf6f7c814e6c269924ea4f15278cd73a96ee61
Dan Brown <danbrown@gmail.com>
2025-10-27 15:27:31 +0100

    move yolo post-process into main function, change detector key

--- edgetpu_tfl_multi.py
+++ edgetpu_tfl_multi.py
@@ -19,18 +19,17 @@ except ModuleNotFoundError:
 
 logger = logging.getLogger(__name__)
 
-DETECTOR_KEY = "edgetpu_yolo9"
+DETECTOR_KEY = "edgetpu_multi"
 
 class EdgeTpuDetectorConfig(BaseDetectorConfig):
     type: Literal[DETECTOR_KEY]
     device: str = Field(default=None, title="Device Type")
 
-
 class EdgeTpuTfl(DetectionApi):
     type_key = DETECTOR_KEY
 
     def __init__(self, detector_config: EdgeTpuDetectorConfig):
-        logger.info(f"Overriding detectors/plugins/edgetpu_tfl.py with host's version intended for Yolo v9")
+        logger.info(f"Initializing {DETECTOR_KEY} detector with multi-model support (YOLO single tensor, SSD)")
         device_config = {}
         if detector_config.device is not None:
             device_config = {"device": detector_config.device}
@@ -66,17 +65,21 @@ class EdgeTpuTfl(DetectionApi):
 
         self.tensor_input_details = self.interpreter.get_input_details()
         self.tensor_output_details = self.interpreter.get_output_details()
-        #self.model_type = detector_config.model.model_type # is this used anywhere?
-        self.model_width = detector_config.model.width
-        self.model_height = detector_config.model.height
+        self.model_width = detector_config.model.width # used for YOLO
+        self.model_height = detector_config.model.height # used for YOLO
 
-        self.min_score = 0.4 # yolorest used 0.25
-        self.max_detections = 20 # seems to be hard-coded elsewhere, needs to be 20 here
+        self.min_score = 0.4 # used for SSD
+        self.max_detections = 20 # used for SSD
 
-        if len(self.tensor_output_details) == 1:  # generic YOLO has 1 output, SSD has 4
+        # Determine model type based on number of outputs
+        if len(self.tensor_output_details) == 1:
+            # Single-tensor YOLO model
             self.yolo_model = True
-        else:
+            logger.info("Detected single-tensor YOLO model")
+        elif len(self.tensor_output_details) == 4:
+            # Legacy SSD model (4 outputs: boxes, class_ids, scores, count)
             self.yolo_model = False
+            logger.info("Detected SSD model with 4 outputs")
             for x in self.tensor_output_details:
                 if len(x["shape"]) == 3:
                     self.output_boxes_index = x["index"]
@@ -85,52 +88,57 @@ class EdgeTpuTfl(DetectionApi):
 
             self.output_class_ids_index = None
             self.output_class_scores_index = None
+        else:
+            raise ValueError(f"Unsupported model with {len(self.tensor_output_details)} outputs")
 
     def determine_indexes_for_non_yolo_models(self):
+        """Legacy method for SSD models."""
         if self.output_class_ids_index is None or self.output_class_scores_index is None:
             for i in range(4):
                 index = self.tensor_output_details[i]["index"]
                 if index != self.output_boxes_index and index != self.output_count_index:
-                    # check if is integer
                     if np.mod(np.float32(self.interpreter.tensor(index)()[0][0]), 1) == 0.0:
                         self.output_class_ids_index = index
                     else:
                         self.output_scores_index = index
 
     def yolo_preprocess(self, input):
+        """some models need UINT8, others need INT8. This will make the necessary transformation"""
         details = self.tensor_input_details[0]
         input = input.astype('float') / 255
         scale, zero_point = details['quantization']
         input = (input / scale + zero_point).astype(details['dtype'])
         return input
 
-    def yolo_postprocess(self):
-        model_width = self.model_width
-        model_height = self.model_height
-        outputs = []
-        for output in self.tensor_output_details:
-            x = self.interpreter.get_tensor(output['index'])
-            scale, zero_point = output['quantization']
-            x = (x.astype(np.float32) - zero_point) * scale
-            # Denormalize xywh by image size
-            # See https://github.com/ultralytics/ultralytics/pull/1695
-            x[:, [0, 2]] *= model_width
-            x[:, [1, 3]] *= model_height
-            outputs.append(x)
-
-        # Use Frigate's optimized post_process_yolo function
-        return post_process_yolo(outputs, model_width, model_height)
-
     def detect_raw(self, tensor_input):
         if self.yolo_model:
+            # Single-tensor YOLO model
+            # model output is a list with shape [84, 756], where the 84 numbers are:
+            # 0,1,2,3 are box coordinates in xywh format normalized to [0,1]
+            # 4: are 80 class probabilities corresponding to the COCO classes
             tensor_input = self.yolo_preprocess(tensor_input)
+            self.interpreter.set_tensor(self.tensor_input_details[0]["index"], tensor_input)
+            self.interpreter.invoke()
 
-        self.interpreter.set_tensor(self.tensor_input_details[0]["index"], tensor_input)
-        self.interpreter.invoke()
+            model_width = self.model_width
+            model_height = self.model_height
+            outputs = []
+            for output in self.tensor_output_details:
+                x = self.interpreter.get_tensor(output['index'])
+                scale, zero_point = output['quantization']
+                x = (x.astype(np.float32) - zero_point) * scale
+                # Denormalize xywh by image size
+                x[:, [0, 2]] *= model_width
+                x[:, [1, 3]] *= model_height
+                outputs.append(x)
 
-        if self.yolo_model:
-            return self.yolo_postprocess()
+            return post_process_yolo(outputs, model_width, model_height)
+
         else:
+            # Default SSD model
+            self.interpreter.set_tensor(self.tensor_input_details[0]["index"], tensor_input)
+            self.interpreter.invoke()
+
             self.determine_indexes_for_non_yolo_models()
             boxes = self.interpreter.tensor(self.tensor_output_details[0]["index"])()[0]
             class_ids = self.interpreter.tensor(self.tensor_output_details[1]["index"])()[0]
@@ -139,22 +147,21 @@ class EdgeTpuTfl(DetectionApi):
                 self.interpreter.tensor(self.tensor_output_details[3]["index"])()[0]
             )
 
-        detections = np.zeros((self.max_detections, 6), np.float32)
+            detections = np.zeros((self.max_detections, 6), np.float32)
 
-        for i in range(count):
-            if scores[i] < self.min_score:
-                break
-            if i == self.max_detections:
-                logger.info(f"Too many detections ({count})!")
-                break
-            detections[i] = [
-                class_ids[i],
-                float(scores[i]),
-                boxes[i][0],
-                boxes[i][1],
-                boxes[i][2],
-                boxes[i][3],
-            ]
+            for i in range(count):
+                if scores[i] < self.min_score:
+                    break
+                if i == self.max_detections:
+                    logger.info(f"Too many detections ({count})!")
+                    break
+                detections[i] = [
+                    class_ids[i],
+                    float(scores[i]),
+                    boxes[i][0],
+                    boxes[i][1],
+                    boxes[i][2],
+                    boxes[i][3],
+                ]
 
-        return detections
-
+            return detections
